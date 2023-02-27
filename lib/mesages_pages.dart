51780a8b2714acb9bfa57c933cee3a2fb59acbc6
eyupGuc <meydanguresi@gmail.com>
2023-02-27 22:27:03 +0300

    message

--- lib/mesages_pages.dart
+++ lib/mesages_pages.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MessagesPage extends StatelessWidget {
@@ -6,24 +8,69 @@ class MessagesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("messages"),
-      ),
-      body: ListView.builder(itemBuilder: ((context, index) {
-        bool benMiyim = true;
-        return Align(
-          alignment: Alignment.centerRight,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
+        appBar: AppBar(
+          title: const Text("messages"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(reverse: true,
+                itemBuilder: ((context, index) {
+                  bool benMiyim = Random().nextBool();
+                  return Align(
+                    alignment:
+                        benMiyim ? Alignment.centerRight : Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 16),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(15)),
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 2,
+                            ),
+                            color: Colors.purple.shade300),
+                        child: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text("Merhaba dostum nerdesin?"),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
               ),
             ),
-            child: Text("Mesaj"),
-          ),
-        );
-      })),
-    );
+            DecoratedBox(
+              decoration: BoxDecoration(border: Border.all()),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25))),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 18.0),
+                          child: TextField(
+                            decoration:
+                                InputDecoration(border: InputBorder.none),
+                          ),
+                        )),
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: ElevatedButton(
+                        onPressed: () {}, child: const Text("Send")),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
